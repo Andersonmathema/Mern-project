4f0e5b55f14d9ff28e414236c669066f00c187d7
Anderson Andrade Silva <ander.math89@gmail.com>
2024-12-30 22:58:49 -0300

    refactor: added the software architecture

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
+const express = require("express");
+const userRoute = require("./src/routes/user.route");
 const app = express();
 
-// Rota
-  //Method HTTP - CRUD(Create, Read, Update, Delete)
-    //GET - pega info
-    //POST - Cria uma info
-    //PUT - Altera toda a info
-    //PATCH - Altera parte da info
-    //DELETE - Apaga uma info
+app.use("/soma", userRoute);
 
-  //Name - Um identificador da rota
+app.listen(3000);
+// Rota
+//Method HTTP - CRUD(Create, Read, Update, Delete)
+//GET - pega info
+//POST - Cria uma info
+//PUT - Altera toda a info
+//PATCH - Altera parte da info
+//DELETE - Apaga uma info
+
+//Name - Um identificador da rota
     
   //Function(callback) - responsável por executar algum comando
   // "Callbacks são funções anônimas chamadas dentro de outra função"
 
-app.get('/soma', (req, res) => {
-  const soma = 1+1;
-  res.send({soma: soma});
-});
-
-app.listen(3000);
-
-
+// app.get('/soma', (req, res) => {
+//   const soma = 1+1;
+//   res.send({soma: soma});
+// });
