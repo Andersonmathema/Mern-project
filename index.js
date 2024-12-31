2b7a863c2bcbea3aab3d65b17f3e59fbcc2b1c06
Anderson Andrade Silva <ander.math89@gmail.com>
2024-12-30 22:18:18 -0300

    Refactor: add route soma

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// Rota
+  //Method HTTP - CRUD(Create, Read, Update, Delete)
+    //GET - pega info
+    //POST - Cria uma info
+    //PUT - Altera toda a info
+    //PATCH - Altera parte da info
+    //DELETE - Apaga uma info
+
+  //Name - Um identificador da rota
+    
+  //Function(callback) - responsável por executar algum comando
+  // "Callbacks são funções anônimas chamadas dentro de outra função"
+
+app.get('/soma', (req, res) => {
+  const soma = 1+1;
+  res.send({soma: soma});
 });
 
 app.listen(3000);
